dd5c7b00226fa6b110a9f613cd32b7f4d3911989
Zielinski.Kamil <215933@edu.p.lodz.pl>
2020-03-17 23:51:53 +0100

    Working logging form with refresh

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -8,7 +8,6 @@
 </head>
 <body>
 	<h1>Hello JSP and Servlet!</h1>
-<!-- 	<h2>Click here to go <a href = "HelloWorld"> My Servlet</a></h2> -->
 <!-- 	 <form action="HelloWorld" method="post"> -->
 <!--         <label for="int1">int1:</label> -->
 <!--         <input type="text" id="int1" name="int1"><br> -->
@@ -16,5 +15,6 @@
 <!--         <input type="text" id="int2" name="int2"><br><br> -->
 <!--         <input type="submit" value="Submit"> -->
 <!--     </form>  -->
+
 </body>
 </html>
--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -10,15 +10,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class HelloWorld
  */
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
+
     private static final long serialVersionUID = 1L;
     private int a;
     private String msg;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,31 +30,82 @@ public class HelloWorld extends HttpServlet {
         msg = config.getInitParameter("my");
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("result of getMethod: " + request.getMethod());
-        writer.println("result of getRemoteAddr: " + request.getRemoteAddr());
-        writer.println("result of getServerName: " + request.getServerName());
-        writer.println("result of getHeader - accept" + request.getHeader("Accept"));
-        writer.println("result of getHeader - Accpet-language" + request.getHeader("Accept-Language"));
-        writer.println("result of getHeader - Accpet-Encoding" + request.getHeader("Accept-Encoding"));
-        writer.println("result of getHeader - User-agent" + request.getHeader("User-Agent"));
-        a++;
-        writer.println(a);
-        writer.println(msg);
-//        ServletConfig cfg = getServletConfig();
-//        ServletContext ctx = getServletContext();
-//        writer.println(cfg.getInitParameter("myParam") + ctx.getInitParameter("myParam"));
-    }
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-        
+        /*
+         * writer.println("result of getMethod: " + request.getMethod()); writer.println("result of getRemoteAddr: " +
+         * request.getRemoteAddr()); writer.println("result of getServerName: " + request.getServerName());
+         * writer.println("result of getHeader - accept" + request.getHeader("Accept"));
+         * writer.println("result of getHeader - Accpet-language" + request.getHeader("Accept-Language"));
+         * writer.println("result of getHeader - Accpet-Encoding" + request.getHeader("Accept-Encoding"));
+         * writer.println("result of getHeader - User-agent" + request.getHeader("User-Agent")); a++; writer.println(a);
+         * writer.println(msg);
+         */
+        // ServletConfig cfg = getServletConfig();
+        // ServletContext ctx = getServletContext();
+        // writer.println(cfg.getInitParameter("myParam") + ctx.getInitParameter("myParam"));
+        HttpSession session = request.getSession(true);
+        String action = request.getParameter("akcja");
+        if (action != null) {
+            if (action.equals("wyloguj")) {
+                Boolean wartosc = false;
+                System.out.println("wylogujINN");
+                session.setAttribute("zalogowany", wartosc);
+            }
+        }
+        Boolean loggedIn = (Boolean) session.getAttribute("zalogowany");
+        if (loggedIn == null) {
+            loggedIn = false;
+            System.out.println("nulled");
+        }
+        if (loggedIn == true) {
+            writer.println("<html>");
+            writer.println("<h1>ZALOGOWANY</h1>");
+            writer.println("<form method=\"get\">");
+            writer.println("<input type=\"hidden\" name=\"akcja\" value=\"wyloguj\" \\>");
+            writer.println("<input type=\"submit\" value=\"Wyloguj\" \\>");
+            writer.println("</form>");
+            writer.println("</html>");
+        } else {
+            String user;
+            String pass;
+            user = request.getParameter("user");
+            pass = request.getParameter("pass");
+            System.out.println("loged1");
+            if (user != null && pass != null) {
+                if (user.equals("radek") && pass.equals("haslo")) {
+                    loggedIn = true;
+                    session.setAttribute("zalogowany", loggedIn);
+                    System.out.println("loged2");
+                    response.setHeader("Refresh", "1");
+                } else {
+                    System.out.println("not loged");
+                    writer.println("<html>");
+                    writer.println("<form method=\"get\">");
+                    writer.println("<input type=\"text\" name=\"user\" \\>");
+                    writer.println("<input type=\"password\" name=\"pass\" \\>");
+                    writer.println("<input type=\"submit\" value=\"zaloguj\" \\>");
+                    writer.println("</form>");
+                    writer.println("</html>");
+                }
+            } else {
+                System.out.println("not loged");
+                writer.println("<html>");
+                writer.println("<form method=\"get\">");
+                writer.println("<input type=\"text\" name=\"user\" \\>");
+                writer.println("<input type=\"password\" name=\"pass\" \\>");
+                writer.println("<input type=\"submit\" value=\"zaloguj\" \\>");
+                writer.println("</form>");
+                writer.println("</html>");
+            }
+        }
     }
 
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+
+    }
 }
