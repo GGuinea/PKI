a40078b7e5acd82a7dca5f724cd8b715e8032daf
Zielinski.Kamil <215933@edu.p.lodz.pl>
2020-03-17 21:22:59 +0100

    Eded with next labs

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -3,6 +3,8 @@ package com.example;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,13 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
+    private int a;
+    private String msg;
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorld() {
-        super();
-        // TODO Auto-generated constructor stub
+
+    public void init(ServletConfig config) throws ServletException {
+        msg = config.getInitParameter("my");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -34,11 +37,12 @@ public class HelloWorld extends HttpServlet {
         writer.println("result of getHeader - Accpet-language" + request.getHeader("Accept-Language"));
         writer.println("result of getHeader - Accpet-Encoding" + request.getHeader("Accept-Encoding"));
         writer.println("result of getHeader - User-agent" + request.getHeader("User-Agent"));
-        if(!(request.getParameter("int1") == null)) {
-            int a = Integer.parseInt(request.getParameter("int2"));
-            int b = Integer.parseInt(request.getParameter("int1"));
-            writer.println(b + "+" + a + "=" + (a+b));
-        }
+        a++;
+        writer.println(a);
+        writer.println(msg);
+//        ServletConfig cfg = getServletConfig();
+//        ServletContext ctx = getServletContext();
+//        writer.println(cfg.getInitParameter("myParam") + ctx.getInitParameter("myParam"));
     }
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
